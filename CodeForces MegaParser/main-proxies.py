98e6d6987f33f4b21fdd5bd9e1da7ad069628885
Rpd-Strike <puscasu.felix1@gmail.com>
2019-08-17 09:31:36 +0300

    correcting last git

--- CodeForces MegaParser/main-proxies.py
+++ CodeForces MegaParser/main-proxies.py
@@ -1,59 +1,129 @@
-import urllib.request
-import json
-import sys
-import time, os, re
-from bs4 import BeautifulSoup
+import urllib.request, urllib.parse
+import json, threading
+import os, time, datetime
+from itertools import cycle
 
-DOWNLOAD_DIR = 'Solutions'
-SUBMISSION_URL = 'http://codeforces.com/contest/{ContestId}/submission/{SubmissionId}'
+from submission_getter import submission_code
+from proxies import get_proxies, get_proxies_manually
 
-EXT = {'C++': 'cpp', 'C': 'c', 'Java': 'java', 'Python': 'py', 'Delphi': 'dpr', 'FPC': 'pas', 'C#': 'cs'}
-EXT_keys = EXT.keys()
+MAX_PROBLEM_SUBS = 1000 * 1000 * 10
+MAX_THREADS = 5
+NUM_PROXIES = 50
+BASE_DIR = 'Surse'
+URL_CONTEST_SUBMISSION = 'http://codeforces.com/api/contest.status?contestId={contestId}'
 
-replacer = {'&quot;': '\"', '&gt;': '>', '&lt;': '<', '&amp;': '&', "&apos;": "'"}
-keys = replacer.keys()
+thread_count = 0
+proxy_pool = cycle(get_proxies(NUM_PROXIES))
 
-def get_ext(comp_lang):
-    if 'C++' in comp_lang:
-        return 'cpp'
-    for key in EXT_keys:
-        if key in comp_lang:
-            return EXT[key]
-    return ""
+def submissions_from_contest(contestId):
+  url = URL_CONTEST_SUBMISSION.format(contestId=contestId)
+  # url = urllib.parse.quote(url)
+  submissions = json.loads(urllib.request.urlopen(url).read())
+  return submissions['result']
 
-def parse(source_code):
-    for key in keys:
-        source_code = source_code.replace(key, replacer[key])
-    return source_code
+def save_progress(sources, contestId):
+  for prob_id, data in sources.items():
+    file_obj = open(BASE_DIR + '/' + str(contestId) + prob_id + '.txt', 'w')
+    json.dump(data, file_obj, indent = 2)
 
-base_dir = DOWNLOAD_DIR
-if not os.path.exists(base_dir):
-    os.makedirs(base_dir)
+def save_contest(contestId):
+  global proxy_pool
+  global thread_count, MAX_PROBLEM_SUBS, BASE_DIR, URL_CONTEST_SUBMISSION
 
-def submission_code(submission, proxy_host):
-    con_id, sub_id = submission['contestId'], submission['id']
-    prob_name, prob_id = submission['problem']['name'], submission['problem']['index']
-    comp_lang = submission['programmingLanguage']
-    submission_full_url = SUBMISSION_URL.format(ContestId=con_id, SubmissionId=sub_id)
-    print ('%s, Fetching submission: %s  proxy: %s' % (str(con_id) + ' ' + prob_id, submission_full_url, proxy_host))
+  print('Thread ' + str(contestId) + ' started')
+  thread_start_time = time.time()
 
-    req = urllib.request.Request(submission_full_url)
-    req.set_proxy(proxy_host, 'http')
+  submissions = submissions_from_contest(contestId)
+  print( 'Contest: ' + str(contestId) + ' has ' + str(len(submissions)) + ' submissions' )
 
-    submission_info = urllib.request.urlopen(req, timeout=10).read()
+  sources, count, subs_checked = {}, 0, 0
+  submissions = sorted(submissions, key = lambda k: k['id'])
+  using_proxy = next(proxy_pool)
+  for sub in submissions:
+    subs_checked = subs_checked + 1
 
-    soup = BeautifulSoup(submission_info, 'html.parser')
-    submission_text = soup.find('pre', id='program-source-text')
-    if submission_text is None:
-        # print ('Could not fectch solution', sub_id)
-        return None
-    result = submission_text.text.replace('\r', '')
-    ext = get_ext(comp_lang)
-    # new_directory = base_dir + '/' + str(con_id) + prob_id
-    # if not os.path.exists(new_directory):
-    #     os.makedirs(new_directory)
-    prob_name = re.sub(r'[\\/*?:"<>|]',"", prob_name)
-    # file = open(new_directory + '/' + str(sub_id) + '.' + ext, 'w')
-    # file.write(result)
-    # file.close()
-    return (result, ext)
+    if sub['verdict'] != 'OK':
+      continue
+
+    count = count + 1
+    if count > MAX_PROBLEM_SUBS:
+      print('Thread ' + str(contestId) + ' reached maximum problem submissions')
+      break
+    
+    percentage = subs_checked / len(submissions) * 100
+
+    prob_id = sub['problem']['index']
+    sub_id = str(sub['id'])
+    if prob_id not in sources.keys():
+      sources[ prob_id ] = {}
+    
+    code, ext = 'nothing', 'no-ext'
+
+    while True:
+      try:
+        code, ext = submission_code(sub, using_proxy)
+        break
+      except Exception as e:
+        print('Thread ' + str(contestId) + ' has Exception, waiting')
+        print(e)
+        time.sleep(4)
+        using_proxy = next(proxy_pool)
+    
+    sources[ prob_id ][ sub_id ] = {
+      'code': code,
+      'ext': ext
+    }
+
+    if count % 10 == 0:
+      save_progress(sources, contestId)
+      print("\nSaved contest {}: {}/{}  {:.2f}%  saved: {}  time: "\
+        .format(contestId, subs_checked, len(submissions), percentage, count) + \
+        time.strftime("%H:%M:%S", time.gmtime(time.time() - thread_start_time)) + "\n")
+
+    if count % 500 == 0:
+      proxy_pool = cycle(get_proxies(NUM_PROXIES))
+
+  thread_count = thread_count - 1
+  print('Thread ' + str(contestId) + ' finished')
+
+def thread_version():
+  global thread_count
+      
+  contest_list = [i + 1 for i in range(1191)]
+  
+  thread_count = 0
+  t_list = []
+  while len(contest_list) > 0:
+    print('thread count: ' + str(thread_count))
+
+    if thread_count < MAX_THREADS:
+      thread_count = thread_count + 1
+      th = threading.Thread(target=save_contest, args=(contest_list[-1],))
+      t_list.append(th)
+      contest_list = contest_list[:-1]
+      th.start()
+    else:
+      for th in t_list:
+        th.join()
+
+    time.sleep(1)
+  
+  print('All work finished! WOW')
+
+def simple_version():
+  print('type contestID:')
+  contestID = int(input())
+  save_contest(contestID)
+
+
+
+def main():
+  new_directory = BASE_DIR
+  if not os.path.exists(new_directory):
+      os.makedirs(new_directory)
+      
+  simple_version()
+
+
+if __name__ == "__main__":
+  main()
